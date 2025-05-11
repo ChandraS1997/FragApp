8b096e3f37c07d2ccc0b4316a440dbcb8abdaf00
Krishna-Ankoliya19 <krishna@hexacoder.com>
2025-05-11 14:48:04 +0530

    Add prettier + ESLint configurations

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+// eslint.config.js or eslint.config.mjs
+
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+import reactNative from 'eslint-plugin-react-native';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    plugins: {
+      react,
+      'react-native': reactNative,
+      prettier: prettierPlugin,
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        __dirname: 'readonly',
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      'react/jsx-uses-vars': 'error',
+      'no-unused-vars': 'warn',
+      'no-console': 'error',
+      'no-debugger': 'error',
+      'prettier/prettier': 'error',
+    },
+  },
+  prettierConfig,
+];
