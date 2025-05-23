451bffe58097c2e301c9dbb9aa6bb8da83c22552
Krishna-Ankoliya19 <krishna@hexacoder.com>
2025-05-23 11:42:20 +0530

    Add slider in logic pop up

--- components/common/Logic.jsx
+++ components/common/Logic.jsx
@@ -1,9 +1,11 @@
 import CustomPopover from './CustomPopover';
 import SidebarButton from './SideBarButton';
 import { Image, Label, YStack } from 'tamagui';
+import { Slider } from 'react-native-elements';
 import SwitchWithLabel from './SwitchWithLabel';
 import CustomMenu from './CustomMenu';
 import logic from '../../assets/icons/logic.svg';
+import { useState } from 'react';
 
 const Logic = () => {
   return (
@@ -23,15 +25,34 @@ const Logic = () => {
 export default Logic;
 
 const LogicContent = () => {
+  const [value, setValue] = useState(50);
   return (
     <>
-      <YStack width={288} justifyContent="space-between">
+      <YStack width={288} gap={8}>
         <SwitchWithLabel label="Advance" size="$2" defaultChecked={false} />
         <YStack justifyContent="space-between">
           <Label size={14} color="$textSecondary">
             Algorithms
           </Label>
           <CustomMenu label="Image Detection by FragAnalysis 4.0" items={['1x', '2x', '3x']} />
+        </YStack>
+        <YStack gap={8}>
+          <Label size={14} color="$textSecondary">
+            Particle Size
+          </Label>
+          <Slider
+            value={value}
+            onValueChange={setValue}
+            minimumValue={0}
+            maximumValue={100}
+            step={1}
+            style={{ height: 6 }}
+            minimumTrackTintColor="#267EF9"
+            maximumTrackTintColor="#F5F5F5"
+            thumbTintColor="#267EF9"
+            thumbStyle={{ height: 20, width: 20 }}
+            trackStyle={{ height: 6, borderRadius: 3 }}
+          />
         </YStack>
       </YStack>
     </>
